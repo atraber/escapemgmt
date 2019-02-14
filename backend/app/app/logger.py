3a9a639c792270d35ace31f5101116725602e87a
Andreas Traber <andytraber@gmail.com>
2019-02-14 17:31:46 +0100

    Change logger to also display process id

--- backend/app/app/logger.py
+++ backend/app/app/logger.py
@@ -6,4 +6,4 @@ import logging
 logger = logging.getLogger('escape-backend')
 logging.basicConfig(
         level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s [%(filename)s:%(lineno)s %(funcName)20s()] %(message)s')
+        format='%(asctime)s %(levelname)s [%(process)d %(filename)s:%(lineno)s %(funcName)20s()] %(message)s')
