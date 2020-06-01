e9ae904425cb48624396a654a5e74d0eb55d9290
Andreas Traber <andytraber@gmail.com>
2020-06-01 15:13:48 +0200

    Work on payments page. Far from finished.

--- frontend/src/app/bookings/payments.component.ts
+++ frontend/src/app/bookings/payments.component.ts
@@ -23,13 +23,38 @@ import {Booking, BookingSource} from '../booking';
 import {BookingsService} from '../bookings.service';
 import {BookingCreateDialog} from './booking-create.dialog';
 
+class Item {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
 @Component({
   templateUrl : './payments.component.html',
   styleUrls : [ './payments.component.scss' ]
 })
 export class PaymentsComponent {
   loaded = false;
+  itemsDataSource = new MatTableDataSource<Item>();
+  items2DataSource = new MatTableDataSource<Item>();
 
   constructor(private bookingsService: BookingsService,
-              private dialog: MatDialog) {}
+              private dialog: MatDialog) {
+    this.loaded = true;
+    this.itemsDataSource.data = [
+      new Item('Raum 1', 100),
+      new Item('Raum 2', 120),
+      new Item('Cola', 3.50),
+      new Item('Bier', 2.10),
+    ];
+
+    this.items2DataSource.data = [
+      new Item('Cash', -50),
+      new Item('Credit Card', -20),
+    ];
+  }
 }
